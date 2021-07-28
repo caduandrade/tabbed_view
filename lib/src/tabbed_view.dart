baf5969c6b1e8774cd6db2c4d69d904e33e36d7c
Carlos Eduardo Leite de Andrade <cadu@caduandrade.net>
2021-07-27 22:01:25 -0300

    Restructuring dart files

--- lib/src/tabbed_view.dart
+++ lib/src/tabbed_view.dart
@@ -6,14 +6,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:tabbed_view/src/content_area.dart';
 import 'package:tabbed_view/src/tab_button.dart';
-import 'package:tabbed_view/src/tab_button_widget.dart';
 import 'package:tabbed_view/src/tab_data.dart';
-import 'package:tabbed_view/src/tab_status.dart';
-import 'package:tabbed_view/src/tab_widget.dart';
 import 'package:tabbed_view/src/tabbed_view_controller.dart';
 import 'package:tabbed_view/src/tabbed_view_data.dart';
-import 'package:tabbed_view/src/tabbed_view_menu_item.dart';
-import 'package:tabbed_view/src/tabs_area_layout.dart';
+import 'package:tabbed_view/src/tabs_area.dart';
 import 'package:tabbed_view/src/theme.dart';
 
 /// Defines a draggable builder for each tab.
@@ -90,7 +86,7 @@ class _TabbedViewState extends State<TabbedView> {
 
   @override
   Widget build(BuildContext context) {
-    Widget tabArea = _TabsArea(data: widget._data);
+    Widget tabArea = TabsArea(data: widget._data);
     ContentArea contentArea = ContentArea(data: widget._data);
     return CustomMultiChildLayout(children: [
       LayoutId(id: 1, child: tabArea),
@@ -133,142 +129,5 @@ class _TabbedViewLayout extends MultiChildLayoutDelegate {
   @override
   bool shouldRelayout(covariant MultiChildLayoutDelegate oldDelegate) {
     return false;
-  }
-}
-
-/// Widget for the tabs and buttons.
-class _TabsArea extends StatefulWidget {
-  const _TabsArea({required this.data});
-
-  final TabbedViewData data;
-
-  @override
-  State<StatefulWidget> createState() => _TabsAreaState();
-}
-
-/// The [_TabsArea] state.
-class _TabsAreaState extends State<_TabsArea> {
-  int? _highlightedIndex;
-
-  final HiddenTabs hiddenTabs = HiddenTabs();
-
-  @override
-  Widget build(BuildContext context) {
-    TabbedViewController controller = widget.data.controller;
-    TabsAreaTheme tabsAreaTheme = widget.data.theme.tabsArea;
-    List<Widget> children = [];
-    for (int index = 0; index < controller.tabs.length; index++) {
-      TabStatus status = _getStatusFor(index);
-      children.add(TabWidget(
-          index: index,
-          status: status,
-          data: widget.data,
-          updateHighlightedIndex: _updateHighlightedIndex));
-    }
-    Widget tabsAreaLayout = TabsAreaLayout(
-        children: children,
-        buttonsAreaBuilder: _buttonsAreaBuilder,
-        theme: widget.data.theme,
-        hiddenTabs: hiddenTabs,
-        selectedTabIndex: controller.selectedIndex);
-    tabsAreaLayout = ClipRect(child: tabsAreaLayout);
-
-    Decoration? decoration;
-    if (tabsAreaTheme.color != null || tabsAreaTheme.border != null) {
-      decoration = BoxDecoration(
-          color: tabsAreaTheme.color, border: tabsAreaTheme.border);
-    }
-    return Container(
-        child: MouseRegion(child: tabsAreaLayout), decoration: decoration);
-  }
-
-  /// Area for buttons like the hidden tabs menu button.
-  ///
-  /// Even if there are no visible buttons, an empty container must be created.
-  Widget _buttonsAreaBuilder(BuildContext context) {
-    ButtonsAreaTheme buttonsAreaTheme = widget.data.theme.tabsArea.buttonsArea;
-    Widget buttonsArea;
-
-    List<TabButton> buttons = [];
-    if (widget.data.tabsAreaButtonsBuilder != null) {
-      buttons = widget.data.tabsAreaButtonsBuilder!(
-          context, widget.data.controller.tabs.length);
-    }
-
-    if (hiddenTabs.hasHiddenTabs) {
-      TabButton hiddenTabsMenuButton = TabButton(
-          icon: buttonsAreaTheme.hiddenTabsMenuButtonIcon,
-          menuBuilder: _hiddenTabsMenuBuilder);
-      buttons.insert(0, hiddenTabsMenuButton);
-    }
-
-    if (buttons.isNotEmpty) {
-      List<Widget> children = [];
-      for (int i = 0; i < buttons.length; i++) {
-        TabButton tabButton = buttons[i];
-        children.add(TabButtonWidget(
-            controller: widget.data.controller,
-            button: tabButton,
-            enabled: true,
-            colors: buttonsAreaTheme.buttonColors,
-            iconSize: buttonsAreaTheme.buttonIconSize));
-      }
-
-      buttonsArea = Row(children: children);
-
-      EdgeInsetsGeometry? margin;
-      if (buttonsAreaTheme.offset > 0) {
-        margin = EdgeInsets.only(left: buttonsAreaTheme.offset);
-      }
-      if (buttonsAreaTheme.decoration != null ||
-          buttonsAreaTheme.padding != null ||
-          margin != null) {
-        buttonsArea = Container(
-            child: buttonsArea,
-            decoration: buttonsAreaTheme.decoration,
-            padding: buttonsAreaTheme.padding,
-            margin: margin);
-      }
-    } else {
-      buttonsArea = Container();
-    }
-    return buttonsArea;
-  }
-
-  /// Builder for hidden tabs menu.
-  List<TabbedViewMenuItem> _hiddenTabsMenuBuilder(BuildContext context) {
-    List<TabbedViewMenuItem> list = [];
-    hiddenTabs.indexes.sort();
-    for (int index in hiddenTabs.indexes) {
-      TabData tab = widget.data.controller.tabs[index];
-      list.add(TabbedViewMenuItem(
-          text: tab.text,
-          onSelection: () => widget.data.controller.selectedIndex = index));
-    }
-    return list;
-  }
-
-  /// Gets the status of the tab for a given index.
-  TabStatus _getStatusFor(int tabIndex) {
-    TabbedViewController controller = widget.data.controller;
-    if (controller.tabs.isEmpty || tabIndex >= controller.tabs.length) {
-      throw Exception('Invalid tab index: $tabIndex');
-    }
-
-    if (controller.selectedIndex != null &&
-        controller.selectedIndex == tabIndex) {
-      return TabStatus.selected;
-    } else if (_highlightedIndex != null && _highlightedIndex == tabIndex) {
-      return TabStatus.highlighted;
-    }
-    return TabStatus.normal;
-  }
-
-  _updateHighlightedIndex(int? tabIndex) {
-    if (_highlightedIndex != tabIndex) {
-      setState(() {
-        _highlightedIndex = tabIndex;
-      });
-    }
   }
 }
---
+++ lib/src/tabs_area.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/widgets.dart';
+import 'package:tabbed_view/src/tab_button.dart';
+import 'package:tabbed_view/src/tab_button_widget.dart';
+import 'package:tabbed_view/src/tab_data.dart';
+import 'package:tabbed_view/src/tab_status.dart';
+import 'package:tabbed_view/src/tab_widget.dart';
+import 'package:tabbed_view/src/tabbed_view_controller.dart';
+import 'package:tabbed_view/src/tabbed_view_data.dart';
+import 'package:tabbed_view/src/tabbed_view_menu_item.dart';
+import 'package:tabbed_view/src/tabs_area_layout.dart';
+import 'package:tabbed_view/src/theme.dart';
+
+/// Widget for the tabs and buttons.
+class TabsArea extends StatefulWidget {
+  const TabsArea({required this.data});
+
+  final TabbedViewData data;
+
+  @override
+  State<StatefulWidget> createState() => _TabsAreaState();
+}
+
+/// The [TabsArea] state.
+class _TabsAreaState extends State<TabsArea> {
+  int? _highlightedIndex;
+
+  final HiddenTabs hiddenTabs = HiddenTabs();
+
+  @override
+  Widget build(BuildContext context) {
+    TabbedViewController controller = widget.data.controller;
+    TabsAreaTheme tabsAreaTheme = widget.data.theme.tabsArea;
+    List<Widget> children = [];
+    for (int index = 0; index < controller.tabs.length; index++) {
+      TabStatus status = _getStatusFor(index);
+      children.add(TabWidget(
+          index: index,
+          status: status,
+          data: widget.data,
+          updateHighlightedIndex: _updateHighlightedIndex));
+    }
+    Widget tabsAreaLayout = TabsAreaLayout(
+        children: children,
+        buttonsAreaBuilder: _buttonsAreaBuilder,
+        theme: widget.data.theme,
+        hiddenTabs: hiddenTabs,
+        selectedTabIndex: controller.selectedIndex);
+    tabsAreaLayout = ClipRect(child: tabsAreaLayout);
+
+    Decoration? decoration;
+    if (tabsAreaTheme.color != null || tabsAreaTheme.border != null) {
+      decoration = BoxDecoration(
+          color: tabsAreaTheme.color, border: tabsAreaTheme.border);
+    }
+    return Container(
+        child: MouseRegion(child: tabsAreaLayout), decoration: decoration);
+  }
+
+  /// Area for buttons like the hidden tabs menu button.
+  ///
+  /// Even if there are no visible buttons, an empty container must be created.
+  Widget _buttonsAreaBuilder(BuildContext context) {
+    ButtonsAreaTheme buttonsAreaTheme = widget.data.theme.tabsArea.buttonsArea;
+    Widget buttonsArea;
+
+    List<TabButton> buttons = [];
+    if (widget.data.tabsAreaButtonsBuilder != null) {
+      buttons = widget.data.tabsAreaButtonsBuilder!(
+          context, widget.data.controller.tabs.length);
+    }
+
+    if (hiddenTabs.hasHiddenTabs) {
+      TabButton hiddenTabsMenuButton = TabButton(
+          icon: buttonsAreaTheme.hiddenTabsMenuButtonIcon,
+          menuBuilder: _hiddenTabsMenuBuilder);
+      buttons.insert(0, hiddenTabsMenuButton);
+    }
+
+    if (buttons.isNotEmpty) {
+      List<Widget> children = [];
+      for (int i = 0; i < buttons.length; i++) {
+        TabButton tabButton = buttons[i];
+        children.add(TabButtonWidget(
+            controller: widget.data.controller,
+            button: tabButton,
+            enabled: true,
+            colors: buttonsAreaTheme.buttonColors,
+            iconSize: buttonsAreaTheme.buttonIconSize));
+      }
+
+      buttonsArea = Row(children: children);
+
+      EdgeInsetsGeometry? margin;
+      if (buttonsAreaTheme.offset > 0) {
+        margin = EdgeInsets.only(left: buttonsAreaTheme.offset);
+      }
+      if (buttonsAreaTheme.decoration != null ||
+          buttonsAreaTheme.padding != null ||
+          margin != null) {
+        buttonsArea = Container(
+            child: buttonsArea,
+            decoration: buttonsAreaTheme.decoration,
+            padding: buttonsAreaTheme.padding,
+            margin: margin);
+      }
+    } else {
+      buttonsArea = Container();
+    }
+    return buttonsArea;
+  }
+
+  /// Builder for hidden tabs menu.
+  List<TabbedViewMenuItem> _hiddenTabsMenuBuilder(BuildContext context) {
+    List<TabbedViewMenuItem> list = [];
+    hiddenTabs.indexes.sort();
+    for (int index in hiddenTabs.indexes) {
+      TabData tab = widget.data.controller.tabs[index];
+      list.add(TabbedViewMenuItem(
+          text: tab.text,
+          onSelection: () => widget.data.controller.selectedIndex = index));
+    }
+    return list;
+  }
+
+  /// Gets the status of the tab for a given index.
+  TabStatus _getStatusFor(int tabIndex) {
+    TabbedViewController controller = widget.data.controller;
+    if (controller.tabs.isEmpty || tabIndex >= controller.tabs.length) {
+      throw Exception('Invalid tab index: $tabIndex');
+    }
+
+    if (controller.selectedIndex != null &&
+        controller.selectedIndex == tabIndex) {
+      return TabStatus.selected;
+    } else if (_highlightedIndex != null && _highlightedIndex == tabIndex) {
+      return TabStatus.highlighted;
+    }
+    return TabStatus.normal;
+  }
+
+  _updateHighlightedIndex(int? tabIndex) {
+    if (_highlightedIndex != tabIndex) {
+      setState(() {
+        _highlightedIndex = tabIndex;
+      });
+    }
+  }
+}
