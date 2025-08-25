e06eaacfd2c5ce73ca8d9c912628377813fc9356
Carlos Eduardo Leite de Andrade <cadu@caduandrade.net>
2025-08-25 18:28:38 -0300

    Removing unnecessary attributes. #54

--- lib/src/internal/tabs_area/tabs_area_layout_parent_data.dart
+++ lib/src/internal/tabs_area/tabs_area_layout_parent_data.dart
@@ -4,20 +4,13 @@ import 'package:meta/meta.dart';
 /// Parent data for [_TabsAreaLayoutRenderBox] class.
 @internal
 class TabsAreaLayoutParentData extends ContainerBoxParentData<RenderBox> {
-
   bool visible = false;
   bool selected = false;
-
-  double leftBorderHeight = 0;
-  double rightBorderHeight = 0;
 
   /// Resets all values.
   void reset() {
     visible = false;
     selected = false;
-
-    leftBorderHeight = 0;
-    rightBorderHeight = 0;
   }
 }
 
